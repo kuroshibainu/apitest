dd837fed6e7acf1d3dd2fae5edabb1c63452ed67
matsumoto <matsumoto@iforza.co.jp>
2016-01-05 13:40:09 +0900

    Weather Project First Commit

---
+++ Weather/Weather/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace webapi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Today = new System.Windows.Forms.Label();
+            this.Tomorrow = new System.Windows.Forms.Label();
+            this.DayAfterTomorrow = new System.Windows.Forms.Label();
+            this.Place = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(159, 86);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(300, 108);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(458, 86);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(300, 108);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(757, 86);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(300, 108);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(159, 202);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox4.Size = new System.Drawing.Size(899, 161);
+            this.textBox4.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(327, 92);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 95);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(626, 92);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(132, 95);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(926, 92);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(132, 95);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(12, 9);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(128, 364);
+            this.listBox1.TabIndex = 10;
+            this.listBox1.SelectedValueChanged += new System.EventHandler(this.listBox1_SelectedValueChanged);
+            // 
+            // Today
+            // 
+            this.Today.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Today.Location = new System.Drawing.Point(159, 54);
+            this.Today.Name = "Today";
+            this.Today.Size = new System.Drawing.Size(300, 33);
+            this.Today.TabIndex = 11;
+            this.Today.Text = "今日";
+            this.Today.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Tomorrow
+            // 
+            this.Tomorrow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Tomorrow.Location = new System.Drawing.Point(458, 54);
+            this.Tomorrow.Name = "Tomorrow";
+            this.Tomorrow.Size = new System.Drawing.Size(300, 33);
+            this.Tomorrow.TabIndex = 12;
+            this.Tomorrow.Text = "明日";
+            this.Tomorrow.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // DayAfterTomorrow
+            // 
+            this.DayAfterTomorrow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.DayAfterTomorrow.Location = new System.Drawing.Point(757, 54);
+            this.DayAfterTomorrow.Name = "DayAfterTomorrow";
+            this.DayAfterTomorrow.Size = new System.Drawing.Size(300, 33);
+            this.DayAfterTomorrow.TabIndex = 13;
+            this.DayAfterTomorrow.Text = "明後日";
+            this.DayAfterTomorrow.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Place
+            // 
+            this.Place.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Place.Font = new System.Drawing.Font("MS UI Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Place.Location = new System.Drawing.Point(159, 9);
+            this.Place.Name = "Place";
+            this.Place.Size = new System.Drawing.Size(899, 46);
+            this.Place.TabIndex = 14;
+            this.Place.Text = "場所";
+            this.Place.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1074, 378);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Place);
+            this.Controls.Add(this.DayAfterTomorrow);
+            this.Controls.Add(this.Tomorrow);
+            this.Controls.Add(this.Today);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "天気予報アプリ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label Today;
+        private System.Windows.Forms.Label Tomorrow;
+        private System.Windows.Forms.Label DayAfterTomorrow;
+        private System.Windows.Forms.Label Place;
+    }
+}
+
---
+++ Weather/Weather/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Codeplex.Data;
+using System.Net;
+
+namespace webapi
+{
+    public partial class Form1 : Form
+    {
+        private const string NO_VALUE = "---";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable dt = CreateDt();
+
+            this.listBox1.SelectedValueChanged -= new System.EventHandler(this.listBox1_SelectedValueChanged);
+
+            this.listBox1.DataSource = dt;
+            this.listBox1.DisplayMember = "name";
+            this.listBox1.ValueMember = "id";
+
+            this.listBox1.SelectedValueChanged += new System.EventHandler(this.listBox1_SelectedValueChanged);
+
+            this.listBox1.SetSelected(0, true);
+
+            this.ActiveControl = this.listBox1;
+        }
+
+        /// <summary>
+        /// listBox1_SelectedValueChanged
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            this.GetWeatherText();
+        }
+
+        private void GetWeatherText()
+        {
+            var url = "http://weather.livedoor.com/forecast/webservice/json/v1?city=" + this.listBox1.SelectedValue;
+            var req = WebRequest.Create(url);
+
+            using (var res = req.GetResponse())
+            using (var s = res.GetResponseStream())
+            {
+                dynamic json = DynamicJson.Parse(s);
+                try
+                {
+                    for (int ix = 0; ix < 3; ix++)
+                    {
+                        //天気(今日)
+                        dynamic today = json.forecasts[ix];
+                        string iconUrl = today.image.url;
+
+                        string dateLabel = today.dateLabel;
+                        string date = today.date;
+                        string telop = today.telop;
+
+                        var sbTempMax = new StringBuilder();
+                        dynamic todayTemperatureMax = today.temperature.max;
+                        if (todayTemperatureMax != null)
+                        {
+                            sbTempMax.AppendFormat("{0}℃", todayTemperatureMax.celsius);
+                        }
+                        else
+                        {
+                            sbTempMax.Append(NO_VALUE);
+                        }
+
+                        var sbTempMin = new StringBuilder();
+                        dynamic todayTemperatureMin = today.temperature.min;
+                        if (todayTemperatureMin != null)
+                        {
+                            sbTempMin.AppendFormat("{0}℃", todayTemperatureMin.celsius);
+                        }
+                        else
+                        {
+                            sbTempMin.Append(NO_VALUE);
+                        }
+
+                        //天気概況文
+                        var situation = "\r\n【天気概況】\r\n" + json.description.text;
+
+                        //area
+                        string location = json.location.prefecture + "・" + json.location.city;
+
+                        string tenki = string.Format("\r\n　　　【天気】　{0}\r\n\r\n【最高気温】　{1}\r\n\r\n【最低気温】　{2}",
+                            telop,
+                            sbTempMax.ToString(),
+                            sbTempMin.ToString()
+                            );
+
+                        this.Place.Text = location;
+
+                        if (ix == 0)
+                        {
+                            this.Today.Text = DateTime.Parse(date).ToLongDateString() + "　（今日）";
+                            this.textBox1.Text = tenki;
+                            this.pictureBox1.ImageLocation = iconUrl;
+                        }
+                        else if (ix == 1)
+                        {
+                            this.Tomorrow.Text = DateTime.Parse(date).ToLongDateString() + "　（明日）";
+                            this.textBox2.Text = tenki;
+                            this.pictureBox2.ImageLocation = iconUrl;
+                        }
+                        else if (ix == 2)
+                        {
+                            this.DayAfterTomorrow.Text = DateTime.Parse(date).ToLongDateString() + "　（明後日）";
+                            this.textBox3.Text = tenki;
+                            this.pictureBox3.ImageLocation = iconUrl;
+                        }
+
+                        this.textBox4.Text = situation;
+                    }
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        /// <summary>
+        /// CreateDt
+        /// </summary>
+        /// <returns>DataTable</returns>
+        private DataTable CreateDt()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("id", typeof(string));
+            dt.Columns.Add("name", typeof(string));
+
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            dict.Add("011000", "稚内");
+            dict.Add("016010", "札幌");
+            dict.Add("020010", "青森");
+            dict.Add("070010", "福島");
+            dict.Add("110010", "さいたま");
+            dict.Add("130010", "東京");
+            dict.Add("180010", "福井");
+            dict.Add("230010", "名古屋");
+            dict.Add("270000", "大阪");
+            dict.Add("310010", "鳥取");
+            dict.Add("390010", "高知");
+            dict.Add("400010", "福岡");
+            dict.Add("460010", "鹿児島");
+            dict.Add("471010", "沖縄");
+
+            foreach ( string id in dict.Keys)
+            {
+                DataRow dr = dt.NewRow();
+                dr["id"] = id;
+                dr["name"] = dict[id];
+                dt.Rows.Add(dr);
+            }
+
+            return dt;
+        }
+
+    }
+}
