ead6585227509daa08520f313cba1e997572b819
yellowrange <develop@yellorange.net>
2016-01-14 18:03:34 +0900

    Rakuten API PG First Commit

---
+++ Test_webapi_Rakuten/webapi/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace webapi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.rbMale = new System.Windows.Forms.RadioButton();
+			this.rbFemale = new System.Windows.Forms.RadioButton();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.nudGeneration = new System.Windows.Forms.NumericUpDown();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.tbDevId = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.nudGeneration)).BeginInit();
+			this.groupBox2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(276, 21);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(219, 85);
+			this.button1.TabIndex = 16;
+			this.button1.Text = "ゴルフ場検索";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(24, 209);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ReadOnly = true;
+			this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox2.Size = new System.Drawing.Size(470, 93);
+			this.textBox2.TabIndex = 17;
+			// 
+			// label1
+			// 
+			this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.label1.Font = new System.Drawing.Font("MS UI Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label1.Location = new System.Drawing.Point(24, 140);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(470, 59);
+			this.label1.TabIndex = 18;
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(24, 309);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.RowHeadersVisible = false;
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+			this.dataGridView1.Size = new System.Drawing.Size(470, 616);
+			this.dataGridView1.TabIndex = 19;
+			this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+			// 
+			// rbMale
+			// 
+			this.rbMale.AutoSize = true;
+			this.rbMale.Location = new System.Drawing.Point(6, 21);
+			this.rbMale.Name = "rbMale";
+			this.rbMale.Size = new System.Drawing.Size(43, 19);
+			this.rbMale.TabIndex = 20;
+			this.rbMale.TabStop = true;
+			this.rbMale.Text = "男";
+			this.rbMale.UseVisualStyleBackColor = true;
+			// 
+			// rbFemale
+			// 
+			this.rbFemale.AutoSize = true;
+			this.rbFemale.Location = new System.Drawing.Point(6, 55);
+			this.rbFemale.Name = "rbFemale";
+			this.rbFemale.Size = new System.Drawing.Size(43, 19);
+			this.rbFemale.TabIndex = 21;
+			this.rbFemale.TabStop = true;
+			this.rbFemale.Text = "女";
+			this.rbFemale.UseVisualStyleBackColor = true;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.rbMale);
+			this.groupBox1.Controls.Add(this.rbFemale);
+			this.groupBox1.Location = new System.Drawing.Point(24, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(81, 94);
+			this.groupBox1.TabIndex = 22;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "性別";
+			// 
+			// nudGeneration
+			// 
+			this.nudGeneration.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+			this.nudGeneration.Location = new System.Drawing.Point(13, 33);
+			this.nudGeneration.Name = "nudGeneration";
+			this.nudGeneration.Size = new System.Drawing.Size(62, 22);
+			this.nudGeneration.TabIndex = 23;
+			this.nudGeneration.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.nudGeneration);
+			this.groupBox2.Location = new System.Drawing.Point(123, 12);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(81, 94);
+			this.groupBox2.TabIndex = 23;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "年代";
+			// 
+			// tbDevId
+			// 
+			this.tbDevId.Location = new System.Drawing.Point(24, 115);
+			this.tbDevId.Name = "tbDevId";
+			this.tbDevId.Size = new System.Drawing.Size(470, 22);
+			this.tbDevId.TabIndex = 24;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(519, 950);
+			this.Controls.Add(this.tbDevId);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.Text = "ヤフーAPIいじり";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.nudGeneration)).EndInit();
+			this.groupBox2.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown nudGeneration;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tbDevId;
+    }
+}
+
