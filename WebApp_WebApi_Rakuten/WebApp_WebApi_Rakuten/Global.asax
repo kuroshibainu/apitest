29c81086bb471fbaec5e76af5536a3e0e9f14303
yellowrange <naoki.matsumoto@yellowrange.net>
2016-01-20 01:03:35 +0900

    WebApp_WebApi_Rakuten First Commit

---
+++ WebApp_WebApi_Rakuten/WebApp_WebApi_Rakuten/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApp_WebApi_Rakuten.MvcApplication" Language="C#" %>
---
+++ WebApp_WebApi_Rakuten/WebApp_WebApi_Rakuten/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApp_WebApi_Rakuten.Startup))]
+namespace WebApp_WebApi_Rakuten
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
